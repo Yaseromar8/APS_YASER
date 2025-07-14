ff240ab872065d4b3d21712266e2fbf7a2228c9a
Yaser Sanchez <92904356+Yaseromar8@users.noreply.github.com>
2025-07-13 23:10:14 -0500

    Add files via upload

---
+++ model/issue.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const AcceptedFacilities = [
+    'montreal',
+    'el'
+];
+
+function validateURN(value) {
+    return value.match(/^[a-zA-Z0-9]{1,128}$/);
+}
+
+function validateFacility(value) {
+    return AcceptedFacilities.indexOf(value) !== -1;
+}
+
+function validateAuthor(value) {
+    return value.match(/^[a-zA-Z ]{1,64}$/);
+}
+
+function validateDescription(value) {
+    return value.match(/^[a-zA-Z.,?!\s]{1,256}$/);
+}
+
+const issueSchema = new mongoose.Schema({
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    urn: {
+        type: String,
+        required: true,
+        validate: validateURN
+    },    
+    facility: {
+        type: String,
+        required: true,
+        validate: validateFacility
+    },
+    author: {
+        type: String,
+        required: true,
+        validate: validateAuthor
+    },
+    text: {
+        type: String,
+        required: true,
+        validate: validateDescription
+    },
+    img: {
+        type: String,
+        validate: value => validator.isURL(value) && value.length < 256
+    },
+    x: {
+        type: Number,
+        required: true
+    },
+    y: {
+        type: Number,
+        required: true
+    },
+    z: {
+        type: Number,
+        required: true
+    },
+    partId: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Issue', issueSchema);
